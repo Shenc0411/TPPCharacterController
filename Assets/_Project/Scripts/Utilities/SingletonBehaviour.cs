c61d90586baefbc740572750ba3e2b3214331114
Cong Shen <shenc0411@gmail.com>
2019-07-03 13:07:50 -0700

    Added SingletonBehaviour

---
+++ Assets/_Project/Scripts/Utilities/SingletonBehaviour.cs
@@ -0,0 +1,24 @@
+﻿namespace Lake.Utilities
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class SingletonBehaviour<T> : MonoBehaviour
+    {
+        private static T instance;
+        private static object mutex = new object();
+
+        public static T Instance
+        {
+            get { return instance; }
+            set
+            {
+                lock(mutex)
+                {
+                    instance = value;
+                }
+            }
+        }
+    }
+}
